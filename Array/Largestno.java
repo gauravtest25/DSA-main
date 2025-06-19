16efb18b6ad879bd5d734d29dbebdfae4e6a6ef4
gauravtest25 <gauravsh2505@gmail.com>
2025-06-19 23:40:58 +0530

    largest

--- Array/Largestno.java
+++ Array/Largestno.java
@@ -1,25 +1,23 @@
 package Array;
 
-
 public class Largestno {
-    public static int getLargest(int numbers[]){
-        int largest=Integer.MIN_VALUE;
-        int smallest=Integer.MAX_VALUE;
-        for(int i=0; i<numbers.length; i++){
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+        // int smallest = Integer.MAX_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
             if (largest < numbers[i]) {
-                largest= numbers[i];
+                largest = numbers[i];
             }
-            if (smallest>numbers[i]) {
-                smallest=numbers[i];
-            }
+            // if (smallest > numbers[i]) {
+                // smallest = numbers[i];
+            // }
         }
-        System.out.println("smallest no is :"+ smallest);
+        // System.out.println("smallest no is :" + smallest);
         return largest;
-        }
+    }
 
     public static void main(String[] args) {
-        int numbers[]={1,2,6,3,5};
-        System.out.println("Largest value is:"+ getLargest(numbers));
+        int numbers[] = { 1, 2, 6, 3, 5 };
+        System.out.println("Largest value is:" + getLargest(numbers));
     }
 }
-
